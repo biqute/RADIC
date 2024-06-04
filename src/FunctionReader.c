872ab3ce094780ac42ef5dbce4498529c0730111
Marcello Faggionato <m.faggionato1@campus.unimib.it>
2024-06-04 10:02:28 +0200

    Add files via upload

--- src/FunctionReader.c
+++ src/FunctionReader.c
@@ -4,18 +4,18 @@
 
 #define ALSA_PCM_NEW_HW_PARAMS_API
 
-int write_bytes_to_binary(const char *filename, unsigned char *data, int size)
+int write_data(const char *filename, int *data, int size)
 {
     FILE *fp = fopen(filename, "wb");
-    
     if (fp == NULL)
     {
         perror("fopen failed");
         return 1;
     }
 
-    size_t written = fwrite(data, sizeof(unsigned char), size, fp);
-    if (written != size)
+    size_t written = fwrite(data, sizeof(int), size/sizeof(int), fp);
+    printf("Number of data written to file: %d\n", written);
+    if (written != size/sizeof(int))
     {
         perror("fwrite failed");
         fclose(fp);
@@ -24,6 +24,7 @@ int write_bytes_to_binary(const char *filename, unsigned char *data, int size)
 
     fclose(fp);
     return 0;
+
 }
 
 int main(int argc, char *argv[]) 
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
     int err;
     int rc, dir = 0;
     char *device = "hw:2";
-    int precision = atoi(argv[1]);
+    int precision = atoi(argv[1]); // Precision to use. It can be 16, 24 or 32
     int rate = 192000;
     int channels = 2;
 
@@ -40,7 +41,7 @@ int main(int argc, char *argv[])
         1 loop means that we capture 512 frames which corresponds to approx. 3 ms of data
         To aquire 1 second of data we have to acquire 375 loops
     */
-    int loops = atoi(argv[2]);  
+    int loops = atoi(argv[2]);       
 
     snd_pcm_t *capture_handle;       // Reference to the sound card
     snd_pcm_hw_params_t *hw_params;  // Information about hardware parameters
@@ -100,37 +101,17 @@ int main(int argc, char *argv[])
     {
         int dimension = frames * 4;
         int dataSize = dimension;
-
-        printf("Dimension is: %d \n", dimension);
-
         int data[dimension*sizeof(int)*loops];
-        /*if (data = NULL) 
-        {
-            fprintf(stderr, "Array not allocated");
-            return 1;
-        }
-        */
 
-        printf("Initial data dimension: %ld\n", sizeof(data));
+        printf("Initial data dimension: %ld\n", sizeof(data)/sizeof(int));
 
         int j = 0;
 
         while (loops > 0)
         {
             loops--;
-
-            /*int *buffer = (int*)malloc(dimension*sizeof(int));
-            if (buffer == NULL)
-            {
-                fprintf(stderr, "Buffer not allocated");
-                return 1;
-            }*/
-
             int buffer[dimension*sizeof(int)];
             int *buf = buffer;
-
-            printf("Initial dimension of the buffer: %ld\n", sizeof(buffer));
-
             rc = snd_pcm_readi(capture_handle, buf, dimension);
 
             if (rc == -EPIPE)
@@ -138,45 +119,20 @@ int main(int argc, char *argv[])
                 perror("Reading failed");
             }
 
-            printf("Frames obtained: %d\n", rc);
-            printf("Buffer dimension: %ld\n", sizeof(buf));
-
             for (int i = 0; i < rc; i++)
             {
                 data[j*dimension + i] = buf[i];
-                printf("%d\n", buf[i]);
             }
-
-            //data[j*dimension] = *buffer; I guess this command doesn't work
-            /*for (int i = 0; i < sizeof(buffer); i++)
-            {
-                //data[j*dimension + i] = buffer[i];
-                printf("%d\n", buffer[i]);
-            }*/
-
-            /*dataSize += dimension;
-            data = realloc(data, dataSize*sizeof(int));
-            if (data == NULL)
-            {
-                fprintf(stderr, "Array not reallocated");
-                return 1;
-            }*/
-
-            printf("ciao7\n");
-
-            //free(buffer);
+            
             j++;
         }
 
         printf("Reading process finished\n");
 
-        /*if (write_bytes_to_binary("data.bin", data, sizeof(data)) != 0)
+        if (write_data("data.txt", data, sizeof(data)) != 0)
         {
             return 1;
         }
-        */
-
-        //free(data);
     }
 
     snd_pcm_close(capture_handle);
