b48e3876f1697012a983a738bf02bf119d87130f
Marcello Faggionato <m.faggionato1@campus.unimib.it>
2024-06-11 16:28:03 +0200

    Add files via upload

--- include/functions.h
+++ include/functions.h
@@ -1,5 +1,14 @@
-void play_sin(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision);
+/* Writing Functions */
+void play_sin(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision);
 
-void play_triangular(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision);
+void play_triangular(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision);
 
-void play_square(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision);
+void play_square(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision);
+
+
+/* Reading Functions */
+int write_data_16(const char *filename, int *data, int size);
+
+int write_data_24(const char *filename, long long *data, int size);
+
+int acquire_data(snd_pcm_t *handle, int frames, int loops, int precision);
