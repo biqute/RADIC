897915ee44050d302b676dba3e93d5ca7d5c2db6
Marcello Faggionato <m.faggionato1@campus.unimib.it>
2024-06-11 10:00:36 +0200

    Add files via upload

---
+++ include/functions.h
@@ -0,0 +1,5 @@
+void play_sin(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision);
+
+void play_triangular(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision);
+
+void play_square(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision);
