553fa2bbb052fff9dbe14d6fd536902a67a2e9c5
Marcello Faggionato <m.faggionato1@campus.unimib.it>
2024-06-11 16:28:24 +0200

    Add files via upload

--- lib/functions.c
+++ lib/functions.c
@@ -4,11 +4,13 @@
 #include <alsa/asoundlib.h>
 
 #define sgn(x) (x < 0) ? -1 : (x > 0)
-#define PI 3.14159265358979323846
 #define ALSA_PCM_NEW_HW_PARAMS_API
 
-void play_sin(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision)
+// ************************* Writing Functions ************************* //
+
+void play_sin(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision)
 {   
+    float PI = 3.14159;
     if (precision == 24)
     {
         char *buffer;
@@ -85,8 +87,9 @@ void play_sin(snd_pcm_t *handle, int frames, int num_frames, int rate, double fr
     }
 }
 
-void play_triangular(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision)
+void play_triangular(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision)
 {
+    float PI = 3.14159;
     if (precision == 24)
     {
         char *buffer;
@@ -163,8 +166,9 @@ void play_triangular(snd_pcm_t *handle, int frames, int num_frames, int rate, do
     }    
 }
 
-void play_square(snd_pcm_t *handle, int frames, int num_frames, int rate, double frequency, int max_amplitude, int precision)
+void play_square(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision)
 {
+    float PI = 3.14159;
     if (precision == 24)
     {
         char *buffer;
@@ -237,4 +241,118 @@ void play_square(snd_pcm_t *handle, int frames, int num_frames, int rate, double
             }
         }
     }  
+}
+
+
+// ************************* Reading Functions ************************* //
+int write_data_16(const char *filename, int *data, int size)
+{
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL)
+    {
+        perror("fopen failed");
+        return 1;
+    }
+
+    size_t written = fwrite(data, sizeof(int), size/sizeof(int), fp);
+    printf("Number of data written to file: %d\n", written);
+    if (written != size/sizeof(int))
+    {
+        perror("fwrite failed");
+        fclose(fp);
+        return 1;
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+int write_data_24(const char *filename, long long *data, int size)
+{
+    FILE *fp = fopen(filename, "wb");
+    if (fp == NULL)
+    {
+        perror("fopen failed");
+        return 1;
+    }
+
+    size_t written = fwrite(data, sizeof(long long), size/sizeof(long long), fp);
+    printf("Number of data written to file: %d\n", written);
+    if (written != size/sizeof(long long))
+    {
+        perror("fwrite failed");
+        fclose(fp);
+        return 1;
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+int acquire_data(snd_pcm_t *handle, int frames, int loops, int precision)
+{
+    if (precision == 24)
+    {
+        int num_samples = frames*loops;
+        long long data[num_samples]; // This will contain all the samples
+        int j, rc = 0;
+
+        while (loops > 0)
+        {
+            loops--;
+            long long buffer[frames];
+            rc = snd_pcm_readi(handle, buffer, frames);
+
+            if (rc == -EPIPE || rc == -EBADFD || rc == -ESTRPIPE) 
+            {
+                perror("Reading failed");
+            }
+
+            for (int i = 0; i < rc; i++)
+            {   
+                data[j*frames + i] = buffer[i];
+            }
+            
+            j++;
+        }
+        
+        printf("\tReading process finished\n");
+
+        if (write_data_24("data.txt", data, sizeof(data)) != 0)
+        {
+            return 1;
+        }
+    }
+    else if (precision == 16)
+    {
+        int num_samples = 2*frames*loops;
+        int data[num_samples]; // This will contain all the samples
+        int j = 0;
+        int rc = 0;
+        while (loops > 0)
+        {
+            loops--;
+            int buffer[frames];
+            rc = snd_pcm_readi(handle, buffer, frames);
+
+            if (rc == -EPIPE || rc == -EBADFD || rc == -ESTRPIPE) 
+            {
+                perror("Reading failed");
+            }
+
+            for (int i = 0; i < rc; i++)
+            {   
+                data[j*frames + i] = buffer[i];
+            }
+            
+            j++;
+        }
+        
+        printf("\tReading process finished\n");
+
+        if (write_data_16("data.txt", data, sizeof(data)) != 0)
+        {
+            return 1;
+        }
+    }
 }
