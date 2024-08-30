f495c9c72244a3a485747a6a9b37706f4de0dc6a
Marcello Faggionato <m.faggionato1@campus.unimib.it>
2024-08-30 12:17:15 +0200

    Add files via upload

--- include/functions.h
+++ include/functions.h
@@ -1,14 +1,20 @@
-/* Writing Functions */
-void play_sin(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision);
+#include <alsa/asoundlib.h>
 
-void play_triangular(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision);
+// Generate a sinusoidal function with specified parameters and assign it to memory areas
+void generate_sine(const snd_pcm_channel_area_t *areas, snd_pcm_uframes_t offset, int count, double *_phase, int amplitude);
 
-void play_square(snd_pcm_t *handle, int frames, int num_frames, double rate, double frequency, int max_amplitude, int precision);
+// Generate a triangular function with specified parameters and assign it to memory areas
+void generate_triangular(const snd_pcm_channel_area_t *areas, snd_pcm_uframes_t offset, int count, double *_phase, int amplitude);
 
+// Recovery of the board if an error occours
+int xrun_recovery(snd_pcm_t *handle, int err);
 
-/* Reading Functions */
-int write_data_16(const char *filename, int *data, int size);
+// Write data to the board
+int write_loop(snd_pcm_t *handle, signed long *samples, snd_pcm_channel_area_t *areas, 
+                unsigned int seconds, char *type, unsigned int amplitude, char *write_type);
 
-int write_data_24(const char *filename, long long *data, int size);
+// Read data from the board
+int read_loop(snd_pcm_t *capture_handle, snd_pcm_uframes_t frames, int loops, long long *data);
 
-int acquire_data(snd_pcm_t *handle, int frames, int loops, int precision);
+// Write an array of data to a file
+int data_to_file(const char *filename, long long *data, int size);
